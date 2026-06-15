51a147b2e28fa61b84bf2d6564e212ca883f71a3
KarenEhab53 <karenehab53@gmail.com>
2026-06-15 10:22:57 +0300

    s3 bucket

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "YOUR_BUCKET_NAME"
-    key            = "assignment/alb-asg/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "YOUR_DYNAMODB_TABLE"
-    encrypt        = true
+    bucket = "terraform-state-karen"
+    key    = "assignment/alb-asg/terraform.tfstate"
+    region = "us-east-1"
   }
 }
